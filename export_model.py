d5b92a854577f4a9af0b10e3b7484c4102ad5095
Mario98 <62114328+MariuszKapica@users.noreply.github.com>
2021-10-11 10:02:22 +0100

    script change

--- export_model.py
+++ export_model.py
@@ -5,9 +5,9 @@ def export(directory, model_name):
     model_directory = os.path.join(directory, "Tensorflow", "workspace", "models", model_name)
     pipeline_config_path = os.path.join(model_directory, "pipeline.config")
     export_directory = os.path.join(directory, "Tensorflow", "workspace", "exported_models", model_name)
-    if os.path.isdir(export_directory):
+    try:
         os.system(f'python exporter_main_v2.py --input_type image_tensor --pipeline_config_path {pipeline_config_path} --trained_checkpoint_dir {model_directory} --output_directory .\exported-models\my_model')
-    else:
+    except Exception:
         os.mkdir(export_directory)
         os.system(
             f'python exporter_main_v2.py --input_type image_tensor --pipeline_config_path {pipeline_config_path} --trained_checkpoint_dir {model_directory} --output_directory .\exported-models\my_model')
